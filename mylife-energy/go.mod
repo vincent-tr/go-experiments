029cec87fc22f8b4b9a3b354b12f6052d909a00d
vincent-tr <vincent.tr@gmail.com>
2023-03-29 10:42:08 +0200

    first draft

---
+++ mylife-energy/go.mod
@@ -0,0 +1,24 @@
+module experiment/mylife-energy
+
+go 1.20
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/goccy/go-yaml v1.10.0 // indirect
+	github.com/gookit/color v1.5.2 // indirect
+	github.com/gookit/config/v2 v2.2.1 // indirect
+	github.com/gookit/goutil v0.6.6 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+)
---
+++ mylife-energy/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
+)
+
+type Config struct {
+	Mongo string `mapstructure:"mongo"`
+	Bus   struct {
+		ServerUrl string `mapstructure:"serverUrl"`
+	} `mapstructure:"bus"`
+}
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v\n", err)
+}
+
+func main() {
+
+	config.WithOptions(config.ParseEnv)
+
+	// add driver for support yaml content
+	config.AddDriver(yaml.Driver)
+
+	err := config.LoadFiles("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	conf := Config{}
+	err = config.Decode(&conf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", conf)
+
+	// add default port if needed
+	serverUrl := conf.Bus.ServerUrl
+	uri, err := url.Parse(serverUrl)
+	if err == nil && uri.Port() == "" {
+		serverUrl += ":1883"
+	}
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(serverUrl)
+	opts.SetClientID("mylife-energy-collector")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	sub(client)
+
+	time.Sleep(30 * time.Second)
+
+	client.Disconnect(250)
+}
+
+func sub(client mqtt.Client) {
+	topic := "+/energy"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s\n", topic)
+}
