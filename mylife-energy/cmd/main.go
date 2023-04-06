ea535918185c3b33343e830ce94e81371be1fb38
vincent-tr <vincent.tr@gmail.com>
2023-04-06 15:14:57 +0200

    wip main service

--- mylife-energy/cmd/main.go
+++ mylife-energy/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	mqtt "mylife-energy/pkg/mqtt"
 	serviceRegistry "mylife-energy/pkg/service"
 
@@ -12,13 +13,29 @@ var logger = log.CreateLogger("main")
 
 type MongoConfig = string
 
+type Measure struct {
+	Id                string  `json:"id"`
+	DeviceClass       string  `json:"device_class"`
+	StateClass        string  `json:"state_class"`
+	UnitOfMeasurement string  `json:"unit_of_measurement"`
+	AccuracyDecimals  int     `json:"accuracy_decimals"`
+	Value             float64 `json:"value"`
+}
+
 type MainService struct {
 }
 
 func (service *MainService) Init() error {
 	mqtt := serviceRegistry.GetService[*mqtt.MqttService]("mqtt")
 	mqtt.Subscribe("+/energy", func(data []byte) {
-		logger.WithField("msg", string(data)).Info("Message")
+
+		measure := Measure{}
+		if err := json.Unmarshal(data, &measure); err != nil {
+			logger.WithFields(log.Fields{"error": err, "data": data}).Error("Error reading JSON")
+			return
+		}
+
+		logger.WithField("measure", measure).Info("Measure")
 	})
 
 	return nil
