bf9ea52404eb1e867b99d8f861024892a5e75acf
vincent-tr <vincent.tr@gmail.com>
2023-06-20 10:34:24 +0200

    task queue

---
+++ mylife-tools-server/services/tasks/queue.go
@@ -0,0 +1,121 @@
+package tasks
+
+import (
+	"errors"
+	"fmt"
+	"mylife-tools-server/log"
+	"sync"
+	"time"
+)
+
+type taskQueueStatus int
+
+const (
+	Running = iota
+	Closing
+	Closed
+)
+
+const queueMaxBuffer = 1024
+
+type taskQueue struct {
+	id     string
+	tasks  chan *task // queuing a nil task indicates exit
+	done   chan struct{}
+	status taskQueueStatus
+	mutex  sync.Mutex
+}
+
+func newTaskQueue(id string) *taskQueue {
+	queue := &taskQueue{
+		id:     id,
+		tasks:  make(chan *task, queueMaxBuffer),
+		done:   make(chan struct{}, 1),
+		status: Running,
+	}
+
+	go queue.workerEntry()
+
+	return queue
+}
+
+func (queue *taskQueue) close() {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
+
+	if queue.status == Closing {
+		logger.WithField("queueId", queue.id).Error("Queue is already closing")
+		return
+	} else if queue.status == Closed {
+		logger.WithField("queueId", queue.id).Error("Queue is already closed")
+		return
+	}
+
+	queue.status = Closing
+	queue.tasks <- nil
+	<-queue.done
+	queue.status = Closed
+}
+
+func (queue *taskQueue) submit(name string, taskImpl Task) error {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
+
+	if queue.status == Closing {
+		return errors.New(fmt.Sprintf("Cannot add tasks while closing on queue '%s'", queue.id))
+	} else if queue.status == Closed {
+		return errors.New(fmt.Sprintf("Cannot add tasks on closed queue '%s'", queue.id))
+	}
+
+	queue.tasks <- newTask(name, taskImpl)
+
+	return nil
+}
+
+func (queue *taskQueue) workerEntry() {
+	for {
+		task := <-queue.tasks
+
+		if task == nil {
+			break
+		}
+
+		task.run(queue)
+	}
+
+	queue.done <- struct{}{}
+}
+
+type task struct {
+	name string
+	impl Task
+}
+
+func newTask(name string, impl Task) *task {
+	return &task{
+		name: name,
+		impl: impl,
+	}
+}
+
+func (t *task) run(queue *taskQueue) {
+	logger.WithFields(log.Fields{"queueId": queue.id, "taskName": t.name}).Debug("Task begin")
+
+	tmr := newTimer()
+	t.impl()
+	elapsed := tmr.elapsed()
+
+	logger.WithFields(log.Fields{"queueId": queue.id, "taskName": t.name, "elapsedMs": elapsed}).Debug("Task end")
+}
+
+type timer struct {
+	begin time.Time
+}
+
+func newTimer() timer {
+	return timer{begin: time.Now()}
+}
+
+func (t *timer) elapsed() float64 {
+	return time.Since(t.begin).Seconds() * 1000
+}
---
+++ mylife-tools-server/services/tasks/service.go
@@ -0,0 +1,97 @@
+package tasks
+
+import (
+	"errors"
+	"fmt"
+	"mylife-tools-server/log"
+	"mylife-tools-server/services"
+)
+
+var logger = log.CreateLogger("mylife:server:tasks")
+
+type Task func()
+
+func init() {
+	services.Register(&taskService{})
+}
+
+type taskService struct {
+	queues map[string]*taskQueue
+}
+
+func (service *taskService) Init() error {
+	service.queues = make(map[string]*taskQueue)
+
+	return nil
+}
+
+func (service *taskService) Terminate() error {
+	for id, _ := range service.queues {
+		service.closeQueue(id)
+	}
+
+	return nil
+}
+
+func (service *taskService) ServiceName() string {
+	return "tasks"
+}
+
+func (service *taskService) Dependencies() []string {
+	return []string{}
+}
+
+func (service *taskService) createQueue(id string) error {
+	if _, exists := service.queues[id]; exists {
+		return errors.New(fmt.Sprintf("Cannot create queue '%s': already exists", id))
+	}
+
+	queue := newTaskQueue(id)
+	service.queues[id] = queue
+
+	return nil
+}
+
+func (service *taskService) closeQueue(id string) error {
+	queue, exists := service.queues[id]
+	if !exists {
+		return errors.New(fmt.Sprintf("Cannot close queue '%s': does not exists", id))
+	}
+
+	queue.close()
+	delete(service.queues, id)
+
+	return nil
+}
+
+func (service *taskService) getQueue(id string) (*taskQueue, error) {
+	queue, exists := service.queues[id]
+	if !exists {
+		return nil, errors.New(fmt.Sprintf("Cannot get queue '%s': does not exists", id))
+	}
+
+	return queue, nil
+}
+
+func getService() *taskService {
+	return services.GetService[*taskService]("sessions")
+}
+
+// Public access
+
+func CreateQueue(id string) error {
+	return getService().createQueue(id)
+}
+
+func CloseQueue(id string) error {
+	return getService().closeQueue(id)
+}
+
+func Submit(id string, taskName string, taskImpl Task) error {
+	queue, err := getService().getQueue(id)
+	if err != nil {
+		return err
+	}
+
+	return queue.submit(taskName, taskImpl)
+}
