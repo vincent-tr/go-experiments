749699e8ad95a7eb0040edd6dadfaac9b84c57be
vincent-tr <vincent.tr@gmail.com>
2023-06-25 10:07:09 +0200

    measure view

--- mylife-energy/cmd/web/measure-view.go
+++ mylife-energy/cmd/web/measure-view.go
@@ -72,7 +72,7 @@ func fetchResults() {
 
 	cursor, err := col.Aggregate(context.TODO(), []bson.M{
 		{"$sort": bson.M{"sensor.sensorId": 1, "timestamp": -1}},
-		{"$group": bson.M{"_id": "$sensor.sensorId", "timestamp": bson.M{"$first": "$timestamp"}, "value": bson.M{"$first": "$value"}}},
+		{"$group": bson.M{"_id": "$sensor.sensorId", "timestamp": bson.M{"$first": "$timestamp"}, "value": bson.M{"$first": "$value"}, "sensor": bson.M{"$first": "$sensor"}}},
 	})
 
 	if err != nil {
@@ -82,7 +82,7 @@ func fetchResults() {
 
 	defer cursor.Close(context.TODO())
 
-	var results []interface{}
+	var results []MongoMeasure
 
 	if err = cursor.All(context.TODO(), &results); err != nil {
 		logger.Error(err)
@@ -91,4 +91,19 @@ func fetchResults() {
 	for _, result := range results {
 		logger.Infof("Item: %+v\n", result)
 	}
+}
+
+type MongoMeasure struct {
+	Id        string      `bson:"_id"`
+	Timestamp time.Time   `bson:"timestamp"`
+	Value     float64     `bson:"value"`
+	Sensor    MongoSensor `bson:"sensor"`
+}
+
+type MongoSensor struct {
+	SensorId          string `bson:"sensorId"`
+	DeviceClass       string `bson:"deviceClass"`
+	StateClass        string `bson:"stateClass"`
+	UnitOfMeasurement string `bson:"unitOfMeasurement"`
+	AccuracyDecimals  int    `bson:"accuracyDecimals"`
 }
