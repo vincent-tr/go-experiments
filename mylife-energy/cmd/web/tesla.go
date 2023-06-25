53be6687c27d19ffc9aab689ab43a6e56aa242b2
vincent-tr <vincent.tr@gmail.com>
2023-06-25 21:43:34 +0200

    add notes

---
+++ mylife-energy/cmd/web/tesla.go
@@ -0,0 +1,3 @@
+// https://pkg.go.dev/github.com/bogosj/tesla#WithBaseURL
+// https://github.com/bogosj/tesla/blob/v1.1.0/examples/manage_car.go
+// https://tesla-api.timdorr.com/api-basics/vehicles
