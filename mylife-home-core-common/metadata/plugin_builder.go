595bc956c33c91907c105d4d87ae71dfaa1a7322
vincent-tr <vincent.tr@gmail.com>
2023-09-14 10:38:34 +0200

    cosmetic

--- mylife-home-core-common/metadata/plugin_builder.go
+++ mylife-home-core-common/metadata/plugin_builder.go
@@ -18,15 +18,6 @@ func MakeBuilder(name string, description string, usage PluginUsage) *PluginBuil
 	}
 }
 
-func (builder *PluginBuilder) AddConfig(name string, description string, valueType ConfigType) *PluginBuilder {
-	_, exists := builder.target.config[name]
-	panics.IsFalse(exists)
-
-	builder.target.config[name] = &ConfigItem{name, description, valueType}
-
-	return builder
-}
-
 func (builder *PluginBuilder) AddState(name string, description string, valueType Type) *PluginBuilder {
 	_, exists := builder.target.members[name]
 	panics.IsFalse(exists)
@@ -43,4 +34,17 @@ func (builder *PluginBuilder) AddAction(name string, description string, valueTy
 	builder.target.members[name] = &Member{name, description, Action, valueType}
 
 	return builder
+}
+
+func (builder *PluginBuilder) AddConfig(name string, description string, valueType ConfigType) *PluginBuilder {
+	_, exists := builder.target.config[name]
+	panics.IsFalse(exists)
+
+	builder.target.config[name] = &ConfigItem{name, description, valueType}
+
+	return builder
+}
+
+func (builder *PluginBuilder) Build() *Plugin {
+	return builder.target
 }
