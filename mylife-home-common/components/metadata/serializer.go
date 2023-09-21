686fce428ebfe217839550c03d46bc95ba6e2072
vincent-tr <vincent.tr@gmail.com>
2023-09-21 15:47:04 +0200

    wip serializer

--- mylife-home-common/components/metadata/serializer.go
+++ mylife-home-common/components/metadata/serializer.go
@@ -1,21 +1,135 @@
 package metadata
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type serializerImpl struct{}
 
 var Seralizer = serializerImpl{}
 
+type netPlugin struct {
+	Module      string               `json:"module"`
+	Name        string               `json:"name"`
+	Description string               `json:"description,omitempty"`
+	Usage       PluginUsage          `json:"usage"`
+	Version     string               `json:"version"`
+	Config      map[string]netConfig `json:"config"`
+	Members     map[string]netMember `json:"members"`
+}
+
+type netConfig struct {
+	Description string     `json:"description,omitempty"`
+	ValueType   ConfigType `json:"valueType"`
+}
+
+type netMember struct {
+	Description string     `json:"description,omitempty"`
+	MemberType  MemberType `json:"memberType"`
+	ValueType   string     `json:"valueType"`
+}
+
+type netComponent struct {
+	Id     string
+	Plugin string `json:"id"`
+}
+
 func (e *serializerImpl) SerializeComponent(component *Component) any {
-	panic("TODO")
+	return safeSerialize(netComponent{
+		Id:     component.Id(),
+		Plugin: component.Plugin(),
+	})
 }
 
 func (e *serializerImpl) DeserializeComponent(data any) *Component {
-	panic("TODO")
+	var net netComponent
+	safeDeserialize(data, &net)
+	return MakeComponent(net.Id, net.Plugin)
 }
 
 func (e *serializerImpl) SerializePlugin(plugin *Plugin) any {
-	panic("TODO")
+	net := netPlugin{
+		Module:      plugin.Module(),
+		Name:        plugin.Name(),
+		Description: plugin.Description(),
+		Usage:       plugin.Usage(),
+		Version:     plugin.Version(),
+		Config:      make(map[string]netConfig),
+		Members:     make(map[string]netMember),
+	}
+
+	for _, name := range plugin.ConfigNames() {
+		configItem := plugin.Config(name)
+		net.Config[name] = netConfig{
+			Description: configItem.Description(),
+			ValueType:   configItem.ValueType(),
+		}
+	}
+
+	for _, name := range plugin.MemberNames() {
+		member := plugin.Member(name)
+		net.Members[name] = netMember{
+			Description: member.Description(),
+			MemberType:  member.MemberType(),
+			ValueType:   member.ValueType().String(),
+		}
+	}
+
+	return safeSerialize(net)
 }
 
 func (e *serializerImpl) DeserializePlugin(data any) *Plugin {
-	panic("TODO")
+	var net netPlugin
+	safeDeserialize(data, net)
+
+	builder := MakePluginBuilder(net.Module, net.Name, net.Description, net.Usage, net.Version)
+
+	for name, configItem := range net.Config {
+		builder.AddConfig(name, configItem.Description, configItem.ValueType)
+	}
+
+	for name, member := range net.Members {
+		valueType, err := ParseType(member.ValueType)
+		if err != nil {
+			panic(err)
+		}
+
+		switch member.MemberType {
+		case State:
+			builder.AddState(name, member.Description, valueType)
+		case Action:
+			builder.AddAction(name, member.Description, valueType)
+		default:
+			panic(fmt.Errorf("unsupported member type '%s'", member.MemberType))
+		}
+	}
+
+	return builder.Build()
+}
+
+func safeSerialize(net any) any {
+	raw, err := json.Marshal(net)
+	if err != nil {
+		panic(err)
+	}
+
+	var value any
+
+	if err := json.Unmarshal(raw, &value); err != nil {
+		panic(err)
+	}
+
+	return value
+}
+
+func safeDeserialize(data any, net any) {
+	raw, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(raw, &net); err != nil {
+		panic(err)
+	}
 }
