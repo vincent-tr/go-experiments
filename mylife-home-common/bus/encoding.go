310c8e7010010f054842c7249f2b5cdbdb17c336
vincent-tr <vincent.tr@gmail.com>
2023-09-21 11:50:15 +0200

    value encoding

--- mylife-home-common/bus/encoding.go
+++ mylife-home-common/bus/encoding.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"mylife-home-common/components/metadata"
 )
 
 type encodingImpl struct{}
@@ -120,4 +122,99 @@ func (e *encodingImpl) write(data any) []byte {
 	}
 
 	return buf.Bytes()
+}
+
+func (e *encodingImpl) WriteValue(typ metadata.Type, value any) []byte {
+	switch realType := typ.(type) {
+	case *metadata.RangeType:
+		return e.writeRange(realType, value.(int64))
+
+	case *metadata.TextType:
+		return e.WriteString(value.(string))
+
+	case *metadata.FloatType:
+		return e.WriteFloat(value.(float64))
+
+	case *metadata.BoolType:
+		return e.WriteBool(value.(bool))
+
+	case *metadata.EnumType:
+		return e.WriteString(value.(string))
+
+	case *metadata.ComplexType:
+		return e.WriteJson(value)
+	}
+
+	panic(fmt.Errorf("Unsupported type %s", typ.String()))
+}
+
+func (e *encodingImpl) ReadValue(typ metadata.Type, raw []byte) any {
+	switch realType := typ.(type) {
+	case *metadata.RangeType:
+		return e.readRange(realType, raw)
+
+	case *metadata.TextType:
+		return e.ReadString(raw)
+
+	case *metadata.FloatType:
+		return e.ReadFloat(raw)
+
+	case *metadata.BoolType:
+		return e.ReadBool(raw)
+
+	case *metadata.EnumType:
+		return e.ReadString(raw)
+
+	case *metadata.ComplexType:
+		return e.ReadJson(raw)
+	}
+
+	panic(fmt.Errorf("Unsupported type %s", typ.String()))
+}
+
+const int8Min int64 = -128
+const int8Max int64 = 127
+const uint8Max int64 = 255
+const int32Min int64 = -2147483648
+const int32Max int64 = 2147483647
+const uint32Max int64 = 4294967295
+
+func (e *encodingImpl) writeRange(typ *metadata.RangeType, value int64) []byte {
+	if typ.Min() >= 0 && typ.Max() <= uint8Max {
+		return e.WriteUInt8(uint8(value))
+	}
+
+	if typ.Min() >= int8Min && typ.Max() <= int8Max {
+		return e.WriteInt8(int8(value))
+	}
+
+	if typ.Min() >= 0 && typ.Max() <= uint32Max {
+		return e.WriteUInt32(uint32(value))
+	}
+
+	if typ.Min() >= int32Min && typ.Max() <= int32Max {
+		return e.WriteInt32(int32(value))
+	}
+
+	panic(fmt.Errorf("Cannot represent range type with min=%d and max=%d because bounds are too big", typ.Min(), typ.Max()))
+}
+
+func (e *encodingImpl) readRange(typ *metadata.RangeType, raw []byte) int64 {
+	if typ.Min() >= 0 && typ.Max() <= uint8Max {
+		return int64(e.ReadUInt8(raw))
+	}
+
+	if typ.Min() >= int8Min && typ.Max() <= int8Max {
+		return int64(e.ReadInt8(raw))
+	}
+
+	if typ.Min() >= 0 && typ.Max() <= uint32Max {
+		return int64(e.ReadUInt32(raw))
+	}
+
+	if typ.Min() >= int32Min && typ.Max() <= int32Max {
+		return int64(e.ReadInt32(raw))
+	}
+
+	panic(fmt.Errorf("Cannot represent range type with min=%d and max=%d because bounds are too big", typ.Min(), typ.Max()))
 }
