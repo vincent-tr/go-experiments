605eea8b085b6c7ef1e029008e23930c946863ce
vincent-tr <vincent.tr@gmail.com>
2025-07-24 16:49:30 +0200

    trend follow

---
+++ strategies/trend_follow.go
@@ -0,0 +1,49 @@
+package strategies
+
+import (
+	"go-experiments/common"
+	"go-experiments/traders/modular"
+	"go-experiments/traders/modular/conditions"
+	"go-experiments/traders/modular/indicators"
+	"time"
+)
+
+func TrendFollow(strategy modular.StrategyBuilder) {
+	strategy.SetFilter(
+		conditions.And(
+			conditions.HistoryComplete(),
+			conditions.NoOpenPositions(),
+
+			conditions.Weekday(time.Tuesday, time.Wednesday, time.Thursday),
+			conditions.ExcludeUKHolidays(),
+			conditions.ExcludeUSHolidays(),
+			conditions.Session(common.LondonSession),
+			conditions.Session(common.NYSession),
+
+			conditions.IndicatorRange(indicators.RSI(14), 30, 70),
+			conditions.Threshold(indicators.ADX(14), 20.0, conditions.Above),
+		),
+	)
+
+	strategy.SetLongTrigger(
+		conditions.And(
+			conditions.PriceThreshold(indicators.EMA(50), conditions.Above),
+			conditions.CrossOver(
+				indicators.EMA(50),
+				indicators.EMA(20),
+				conditions.CrossOverUp,
+			),
+		),
+	)
+
+	strategy.SetShortTrigger(
+		conditions.And(
+			conditions.PriceThreshold(indicators.EMA(50), conditions.Below),
+			conditions.CrossOver(
+				indicators.EMA(50),
+				indicators.EMA(20),
+				conditions.CrossOverDown,
+			),
+		),
+	)
+}
